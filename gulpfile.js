253897a17507acad7726648ee5e73e153f4e7310
Greg G <aedotj@users.noreply.github.com>
2016-02-20 23:29:03 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
 var gulp = require('gulp');
+var jade = require('jade');
+var stylus = require('stylus');
 
-gulp.task('default', function () {
+// Jade filters
+jade.filters.typescript = function (str) {
+    
+};
 
+jade.filters.stylus = function (str) {
+    var css;
+    stylus.render(str, { }, function (err, _css) {
+        css = _css;
+    });
+    return css;
+};
+
+// Misc functions
+var renderJade = function (filename, string) {
+    var src = require('stream').Readable({ objectMode: true });
+    src._read = function () {
+        string = jade.renderFile(filename);
+        this.push(new gutil.File({ cwd: "", base: "", path: filename, contents: new Buffer(string) }));
+        this.push(null);
+    };
+    return src;
+};
+
+gulp.task('default', ['jade'], function () {
+    
+});
+
+gulp.task('jade', function () {
+    return renderJade('src/app.jade').pipe(gulp.dest('build/index.html'));
 });
